6235e07bf47a7d935f8329b94e4d569746c623a7
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2025-08-11 17:32:55 -0400

    Fixed issues and add more content

--- portifolio/src/app/pages/about/about.component.ts
+++ portifolio/src/app/pages/about/about.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  
+  skillset: String[] =[
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+   ]
+
+  toolset: String[] = [
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/windows11/windows11-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg',
+  ]
 }
